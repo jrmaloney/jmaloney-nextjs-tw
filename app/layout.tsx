0d2956c9db5b56fb7c87b33a5605280515b86820
John <jrmaloney@gmail.com>
2025-02-06 16:10:50 -0500

    rename footer component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
+import Footer from "@/components/Footer-test";
 import "./globals.css";
 
 const geistSans = Geist({
