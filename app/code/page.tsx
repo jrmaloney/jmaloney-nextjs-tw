0fb4a00dca54ad9307937995a30680d8ae4897c9
John <jrmaloney@gmail.com>
2025-02-18 15:52:34 -0500

    escape ' with &apos;

--- app/code/page.tsx
+++ app/code/page.tsx
@@ -9,10 +9,10 @@ const CodePage = () => {
 
         shortDesc={
           <>
-            Lately I've been instested in Infrastructure as Code (IaC) using 
+            Lately I&apos;ve been instested in Infrastructure as Code (IaC) using 
             <a href="https://developer.hashicorp.com/terraform/install"  target="_blank" className="text-blue-500">Terraform</a> and 
             <a href="  https://app.terraform.io/" target="_blank" className="text-blue-500">Terraform Cloud</a> to provision resources in Azure and AWS. The runbooks below are templates and the steps needed to provision resources I regularly use. 
-            I've included instructions and links to the code in my <a href="https://github.com/jrmaloney" target="_blank" className="text-blue-500">Github</a> account, I hope you find them usefull.
+            I&apos;ve included instructions and links to the code in my <a href="https://github.com/jrmaloney" target="_blank" className="text-blue-500">Github</a> account, I hope you find them usefull.
           </>
         }
         
